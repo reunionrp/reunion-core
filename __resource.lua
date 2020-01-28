2bca878c6e517b21ee56be878ab53671fa22f591
reunionrp <60327182+reunionrp@users.noreply.github.com>
2020-01-28 22:19:34 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,7 @@
+client_scripts {
+	'client/seatbelt.lua',
+        'client/npc_control.lua',
+        'client/idoverhead.lua',
+        'client/disablerewards.lua',
+        'client/visualsettings.lua',
+}
---
+++ client/seatbelt.lua
@@ -0,0 +1,115 @@
+Cfg             = {}
+Cfg.DiffTrigger = 0.255 
+Cfg.MinSpeed    = 30.25 --THIS IS IN m/s
+
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+local speedBuffer  = {}
+local velBuffer    = {}
+local beltOn       = false
+local wasInCar     = false
+local sentInfo	   = false
+
+IsCar = function(veh)
+		    local vc = GetVehicleClass(veh)
+		    return (vc >= 0 and vc <= 7) or (vc >= 9 and vc <= 12) or (vc >= 17 and vc <= 20)
+        end	
+
+Fwv = function (entity)
+		    local hr = GetEntityHeading(entity) + 90.0
+		    if hr < 0.0 then hr = 360.0 + hr end
+		    hr = hr * 0.0174533
+		    return { x = math.cos(hr) * 2.0, y = math.sin(hr) * 2.0 }
+      end
+
+Citizen.CreateThread(function()
+	Citizen.Wait(500)
+	while true do
+		
+		local ped = GetPlayerPed(-1)
+		local car = GetVehiclePedIsIn(ped)
+		
+		if car ~= 0 and (wasInCar or IsCar(car)) then
+			if not sentInfo then
+				sentInfo = true
+				local keyTxt = 'Z'
+				exports.pNotify:SetQueueMax("vehicle", "7")
+				exports.pNotify:SendNotification({
+				  text = "Press <span style='color: #00ff00'>"..keyTxt.."</span> to toggle your seat belt",
+				  type = "info",
+				  queue = "vehicle",
+				  timeout = 2500,
+				  layout = "Centerright"
+				})
+			end
+		else
+			sentInfo = false
+		end
+		
+		if car ~= 0 and (wasInCar or IsCar(car)) then
+		
+			wasInCar = true
+			
+			if beltOn then DisableControlAction(0, 75) end
+			
+			speedBuffer[2] = speedBuffer[1] or 0
+			speedBuffer[1] = GetEntitySpeed(car)
+			--print(tostring(speedBuffer[2] - speedBuffer[1]))
+			
+			if speedBuffer[2] ~= nil 
+			   and not beltOn
+			   and GetEntitySpeedVector(car, true).y > 1.0  
+			   and speedBuffer[1] > Cfg.MinSpeed 
+			   and (speedBuffer[2] - speedBuffer[1]) > (speedBuffer[1] * Cfg.DiffTrigger) then
+			   
+				local co = GetEntityCoords(ped)
+				local fw = Fwv(ped)
+				SetEntityCoords(ped, co.x + fw.x, co.y + fw.y, co.z - 0.47, true, true, true)
+				SetEntityVelocity(ped, velBuffer[2].x, velBuffer[2].y, velBuffer[2].z)
+				Citizen.Wait(1)
+				SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
+			end
+				
+			velBuffer[2] = velBuffer[1]
+			velBuffer[1] = GetEntityVelocity(car)
+				
+			if IsControlJustReleased(0, Keys["Z"]) then
+				beltOn = not beltOn				  
+				if beltOn then 
+					exports.pNotify:SetQueueMax("vehicle", "7")
+					exports.pNotify:SendNotification({
+					  text = "You have put <span style='color: #00ff00'>ON</span> your seat belt",
+					  type = "info",
+					  queue = "vehicle",
+					  timeout = 2500,
+					  layout = "Centerright"
+					})
+				else 
+					exports.pNotify:SetQueueMax("vehicle", "7")
+					exports.pNotify:SendNotification({
+					  text = "You have <span style='color: #ff0000'>REMOVED</span> your seat belt",
+					  type = "info",
+					  queue = "vehicle",
+					  timeout = 2500,
+					  layout = "Centerright"
+					})
+				end 
+			end
+			
+		elseif wasInCar then
+			wasInCar = false
+			beltOn = false
+			speedBuffer[1], speedBuffer[2] = 0.0, 0.0
+		end
+		Citizen.Wait(0)
+	end
+end)
