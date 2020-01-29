925d3bb8386f9e6caf3b79103c46526bb60a8453
reunionrp <60327182+reunionrp@users.noreply.github.com>
2020-01-29 21:00:47 +0200

    Add files via upload

---
+++ client/policeveh.lua
@@ -0,0 +1,14 @@
+
+
+-- VÕimalik, et eemaldab kõik läheduses olevad politsei autod
+
+
+
+Citizen.CreateThread(function()
+    while true do
+    Citizen.Wait(0)
+    local playerPed = GetPlayerPed(-1)
+    local playerLocalisation = GetEntityCoords(playerPed)
+    ClearAreaOfCops(playerLocalisation.x, playerLocalisation.y, playerLocalisation.z, 1000.0)
+    end
+end)
