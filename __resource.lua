d9055f0c0d4151a86cf6adb61f3f60b527db660a
reunionrp <60327182+reunionrp@users.noreply.github.com>
2020-01-31 20:18:14 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,4 @@
 client_scripts {
 	'client/seatbelt.lua',
-        'client/npc_control.lua',
-        'client/idoverhead.lua',
         'client/disablerewards.lua',
-        'client/policeveh.lua',
 }
